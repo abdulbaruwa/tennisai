643585a0e7c75f9fb85bf06fdf4aac20f0672db2
Shangoman <abdulbaruwa@hotmail.com>
2017-12-13 23:19:37 +0000

    update for coninued work on main

--- lib/main.dart
+++ lib/main.dart
@@ -6,78 +6,192 @@ import './tabs/settings.dart' as _thirdTab;
 import './pages/about.dart' as _aboutPage;
 import './pages/support.dart' as _supportPage;
 
-
 void main() => runApp(new MaterialApp(
-  title: 'Tennis Ai',
-  theme: new ThemeData(primarySwatch: Colors.blueGrey, scaffoldBackgroundColor: Colors.white,primaryColor: Colors.blueGrey,backgroundColor: Colors.white),
-  home: new Tabs(),
-onGenerateRoute: (RouteSettings settings){
-  switch(settings.name){
-    case '/about': return new FromRightToLeft(builder: (_) => new _aboutPage.About(),settings: settings,);
-    case '/support': return new FromRightToLeft(builder: (_) => new _supportPage.Support(), settings: settings);
-  }
-},
-));
+      title: 'Tennis Ai',
+      theme: new ThemeData(
+          primarySwatch: Colors.blueGrey,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: Colors.blueGrey,
+          backgroundColor: Colors.white),
+      home: new Tabs(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/about':
+            return new FromRightToLeft(
+              builder: (_) => new _aboutPage.About(),
+              settings: settings,
+            );
+          case '/support':
+            return new FromRightToLeft(
+                builder: (_) => new _supportPage.Support(), settings: settings);
+        }
+      },
+    ));
 
+class FromRightToLeft<T> extends MaterialPageRoute<T>{
+  FromRightToLeft({WidgetBuilder builder, RouteSettings settings}) : super(builder: builder, settings: settings);
 
-class Tabs extends StatefulElement{
   @override
-  TabsState createStat() => new TabState();
+  Widget buildTransitions(BuildContext context,
+  Animation<double> animation, Animation<double> secondaryAnimation,
+  Widget child){
+    if(settings.isInitialRoute) return child;
+
+    return new SlideTransition(child: new Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.black26,
+              blurRadius: 25.0,
+            )
+          ]
+        ),
+        child: child,
+      ),
+    position: new FractionalOffsetTween(
+      begin: const FractionalOffset(1.0, 1.0),
+      end: FractionalOffset.topLeft
+      )
+      .animate(
+        new CurvedAnimation(
+          parent: animation, 
+          curve: Curves.fastOutSlowIn)),);
+
+    @override Duration get transitionDuration => const Duration(milliseconds: 400);
+  }
 }
 
-class TabsState extends State<Tabs>{
+class Tabs extends StatefulWidget {
+  @override
+  TabsState createState() => new TabsState();
+}
 
-  PageController _tabController
+class TabsState extends State<Tabs> {
+  PageController _tabController;
   var _title_app = null;
   int _tab = 0;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _tabController = new PageController();
     this._title_app = TabItems[0].title;
   }
 
-@override 
-void dispose(){
-  super.dispose();
-  _tabController.dispose();
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
 
-}
-
-@override
-Widget build (BuildContext buildcontext) => new Scaffold(
-    //App Bar
-    appBar: new AppBar(
-      title: new Text(
-        _title_app, 
-        style: new TextStyle(
-          fontSize: Theme.of(context).platform == TargetPlatform.iOS ? 17.0 : 20.0,
+  @override
+  Widget build(BuildContext buildcontext) => new Scaffold(
+      //App Bar
+      appBar: new AppBar(
+        title: new Text(
+          _title_app,
+          style: new TextStyle(
+            fontSize:
+                Theme.of(context).platform == TargetPlatform.iOS ? 17.0 : 20.0,
+          ),
         ),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-    ),
+      body: new PageView(
+        controller: _tabController,
+        onPageChanged: onTabChanged,
+        children: <Widget>[
+          new _firstTab.Home(),
+          new _secondTab.Dashboard(),
+          new _thirdTab.Settings()
+        ],
+      ),
 
-  body: new PageView(
-    controller: _tabController,
-    onPageChanged: onTabChanged,
-    children: <Widget>[
-      new _firstTab.Home(),
-      new _secondTab.Dashboard(),
-      new _thirdTab.Settings()
-    ],
-  ),
+      // Tabs
+      bottomNavigationBar: Theme.of(context).platform == TargetPlatform.iOS
+          ? new CupertinoTabBar(
+              activeColor: Colors.blueGrey,
+              currentIndex: _tab,
+              onTap: onTap,
+              items: TabItems.map((TabItem) {
+                return new BottomNavigationBarItem(
+                  title: new Text(TabItem.title),
+                  icon: new Icon(TabItem.icon),
+                );
+              }).toList(),
+            )
+          : new BottomNavigationBar(
+              currentIndex: _tab,
+              onTap: onTap,
+              items: TabItems.map((TabItem) {
+                return new BottomNavigationBarItem(
+                    title: new Text(TabItem.title),
+                    icon: new Icon(TabItem.icon));
+              }).toList(),
+            ),
 
-  // Tabs
-  bottomNavigationBar: Theme.of(context).platform == TargetPlatform.iOS ? new CupertinoTabBar(
-    activeColor: Colors.blueGrey,
-    currentIndex: _tab,
-    onTap: onTap,
-    items: TabItems.map((TabItem){
-      return new BottomNavigationBarItem(title: new Text(TabItem.title),icon: new Icon(TabItem.icon),);
-    }).toList(),
-  ):new BottomNavigationBar(currentIndex: _tab,onTap: onTap,items: TabItems.map((TabItem){return new BottomNavigationBarItem(title: new Text(TabItem.title),icon:new Icon(TabItem.icon));}).toList(),),
-); 
+      //Drawer
+      drawer: new Drawer(
+          child: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 120.0,
+            child: new DrawerHeader(
+              padding: new EdgeInsets.all(0.0),
+              decoration: new BoxDecoration(
+                color: new Color(0xFFECEFF1),
+              ),
+              child: new Center(
+                child: new FlutterLogo(
+                  colors: Colors.blueGrey,
+                  size: 54.0,
+                ),
+              ),
+            ),
+          ),
+          new ListTile(
+              leading: new Icon(Icons.chat),
+              title: new Text('Support'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed('/support');
+              }),
+          new ListTile(
+              leading: new Icon(Icons.info),
+              title: new Text('About'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed('/about');
+              }),
+          new Divider(),
+          new ListTile(
+              leading: new Icon(Icons.exit_to_app),
+              title: new Text('Sign Out'),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+        ],
+      )));
+  void onTap(int tab) {
+    _tabController.jumpToPage(tab);
+  }
+
+  void onTabChanged(int tab) {
+    setState(() {
+      this._tab = tab;
+    });
+    switch (tab) {
+      case 0:
+        this._title_app = TabItems[0].title;
+        break;
+      case 1:
+        this._title_app = TabItems[1].title;
+        break;
+      case 2:
+        this._title_app = TabItems[2].title;
+        break;
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -186,9 +300,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-
 class TabItem {
-  const TabItem({ this.title, this.icon });
+  const TabItem({this.title, this.icon});
   final String title;
   final IconData icon;
 }
