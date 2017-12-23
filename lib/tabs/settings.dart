040e99007cd9ca4f4c683c5d658243a41cbb26ac
Abdul Baruwa <abdulbaruwa@hotmail.com>
2017-12-23 21:18:08 +0000

    Making UserProfile editing possible

--- lib/tabs/settings.dart
+++ lib/tabs/settings.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+final ThemeData _kTheme = new ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.teal,
+  accentColor: Colors.redAccent,
+);
+
 class SettingsTab extends StatefulWidget {
   SettingsTab({Key key}) : super(key: key);
   @override
@@ -7,40 +13,52 @@ class SettingsTab extends StatefulWidget {
 }
 
 class SettingsTabState extends State<SettingsTab> {
-  static final GlobalKey<ScaffoldState> _scaffoldKey =
+  static final GlobalKey<ScaffoldState> scaffoldKey =
       new GlobalKey<ScaffoldState>();
   @override
-  Widget build(BuildContext context) => new Container(
-          child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        
-        children: <Widget>[
-          new Padding(
-              padding: const EdgeInsets.only(left:5.0, top: 5.0), child: new _UserProfile()),
-          new Icon(Icons.search, size: 150.0, color: Colors.black12),
-          new Text('Profile & Settings tab content')
-        ],
-      ));
+  Widget build(BuildContext context) {
+    return new Theme(
+        data: _kTheme.copyWith(platform: Theme.of(context).platform),
+        child: new Scaffold(
+            key: scaffoldKey,
+            floatingActionButton: new FloatingActionButton(
+              child: const Icon(Icons.edit),
+              onPressed: () {
+                scaffoldKey.currentState.showSnackBar(const SnackBar(
+                  content: const Text('Not supported.'),
+                ));
+              },
+            ),
+            body: new Container(
+                child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new _ProfileSection(),
+                new Icon(Icons.search, size: 150.0, color: Colors.black12),
+                new Text('Profile & Settings tab content')
+              ],
+            ))));
+  }
 }
 
-class _ProfileSection extends StatelessWidget{
-  const _ProfileSection ({Key key}) : super(key: key);
+class _ProfileSection extends StatelessWidget {
+  const _ProfileSection({Key key}) : super(key: key);
 
   @override
-   Widget build(BuildContext context) => new Container(
-     color: Colors.amber,
-     child: new SizedBox(
-       height: 200.0,
+  Widget build(BuildContext context) => new Container(
+      color: Colors.amber,
+      child: new SizedBox(
+          height: 105.0,
           child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        
-        children: <Widget>[
-          new Padding(
-              padding: const EdgeInsets.only(left:5.0, top: 5.0), child: new _UserProfile()),
-        ],
-      )));
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Padding(
+                  padding: const EdgeInsets.only(left: 5.0, top: 5.0),
+                  child: new _UserProfile()),
+            ],
+          )));
 }
 
 // Displays the Vendor's name and avatar.
@@ -60,18 +78,40 @@ class _UserProfile extends StatelessWidget {
           const SizedBox(width: 8.0),
           new Expanded(
             child: new Padding(
-              padding: const EdgeInsets.only(left: 2.0, top: 25.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                new Text('Ademola Baruwa'),
-                new Text('66 West Farm Avenue')
-              ],
-            )),
+                padding: const EdgeInsets.only(left: 2.0, top: 25.0),
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Text('Ademola Baruwa'),
+                    new Text('66 West Farm Avenue')
+                  ],
+                )),
           )
         ],
       ),
     );
   }
+}
+
+class UserProfileEdit extends StatefulWidget {
+  UserProfileEdit({Key key}) : super(key: key);
+  @override
+  UserProfileEditState createState() => new UserProfileEditState();
+}
+
+class UserProfileEditState extends State<UserProfileEdit> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      new GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) => new Container(
+          child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new _ProfileSection(),
+          new Icon(Icons.search, size: 150.0, color: Colors.black12),
+          new Text('Edit Profile & Settings tab content')
+        ],
+      ));
 }
