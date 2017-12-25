2dc064002010f0e5d2405746a77846f218dd20a7
Abdul Baruwa <abdulbaruwa@hotmail.com>
2017-12-25 10:51:25 +0000

    Settings updates, create seperate widget for label & selector combos

--- lib/tabs/settings.dart
+++ lib/tabs/settings.dart
@@ -6,6 +6,23 @@ final ThemeData _kTheme = new ThemeData(
   accentColor: Colors.redAccent,
 );
 
+final BoxDecoration _underlineBoxDecoration = new BoxDecoration(
+  border: new Border(
+    bottom: const BorderSide(color: const Color(0xFFD9D9D9)),
+  ),
+);
+
+final BoxDecoration _topBottomBoxDecoration = new BoxDecoration(
+  border: new Border(
+    bottom: const BorderSide(color: const Color(0xFFD9D9D9)),
+    top: const BorderSide(color: const Color(0xFFD9D9D9)),
+  ),
+);
+
+final List<int> _distancesInMiles = [10, 30, 50, 100, 200, 500];
+
+final List<int> _grades = [1, 2, 3, 4, 5, 6];
+
 enum DismissDialogAction {
   cancel,
   discard,
@@ -113,10 +130,59 @@ class UserProfileEdit extends StatefulWidget {
   UserProfileEditState createState() => new UserProfileEditState();
 }
 
+class _LabelIntDropDownItem extends StatelessWidget {
+  _LabelIntDropDownItem(
+      {Key key,
+      this.displayFunc,
+      this.displayIntItems,
+      this.label,
+      this.output})
+      : super(key: key);
+  var displayFunc;
+  final label;
+  List<int> displayIntItems;
+  int output;
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Text(label),
+        new Expanded(
+            child: new Container(
+                alignment: Alignment.bottomRight,
+                child: new DropdownButtonHideUnderline(
+                  child: new Container(
+                    // decoration: _underlineBoxDecoration,
+                    child: new DropdownButton<int>(
+                      items: displayIntItems.map((int value) {
+                        return new DropdownMenuItem<int>(
+                          value: value,
+                          child: new Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: new Text(displayFunc(value)),
+                          ),
+                        );
+                      }).toList(),
+                      value: output,
+                      onChanged: (int value) {
+                        output = value;
+                      },
+                    ),
+                  ),
+                )))
+      ],
+    );
+  }
+}
+
 class UserProfileEditState extends State<UserProfileEdit> {
   static final GlobalKey<ScaffoldState> _scaffoldKey =
       new GlobalKey<ScaffoldState>();
   int quantity = 0;
+  int selectedGender = 0;
+  int miles = 30;
+  int selectedMiles = 30;
+  int selectedGrade = 3;
   final ValueChanged<int> onChanged;
 
   @override
@@ -124,6 +190,7 @@ class UserProfileEditState extends State<UserProfileEdit> {
     final ThemeData theme = Theme.of(context);
 
     return new Scaffold(
+        // backgroundColor: Colors.white70,
         appBar: new AppBar(title: const Text('Edit Profile'), actions: <Widget>[
           new FlatButton(
               child: new Text('SAVE',
@@ -133,78 +200,86 @@ class UserProfileEditState extends State<UserProfileEdit> {
               })
         ]),
         body: new Form(
-          child: new ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: new ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 1.0),
           children: <Widget>[
             new SizedBox(
               height: 96.0,
-              child: new Row(
-                children: <Widget>[
-                  new CircleAvatar(
-                    radius: 36.0,
-                    backgroundImage: const AssetImage('images/ademola.jpg'),
-                  ),
-                  const SizedBox(width: 8.0),
-                  new Expanded(
-                    child: new Padding(
-                        padding: const EdgeInsets.only(
-                            left: 2.0, top: 10.0, right: 15.0),
-                        child: new Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            new TextFormField(
-                              decoration: const InputDecoration(
-                                  hintText: 'First name *', isDense: true),
-                              keyboardType: TextInputType.text,
-                            ),
-                            new TextFormField(
-                              decoration: const InputDecoration(
-                                  hintText: 'Last name *', isDense: true),
-                              keyboardType: TextInputType.text,
-                            ),
-                          ],
-                        )),
-                  )
-                ],
-              ),
+              child: new Container(
+                  color: Colors.white,
+                  child: new Row(
+                    children: <Widget>[
+                      new CircleAvatar(
+                        radius: 36.0,
+                        backgroundImage: const AssetImage('images/ademola.jpg'),
+                      ),
+                      const SizedBox(width: 8.0),
+                      new Expanded(
+                        child: new Padding(
+                            padding: const EdgeInsets.only(
+                                left: 2.0, top: 10.0, right: 15.0),
+                            child: new Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                new TextFormField(
+                                  decoration: const InputDecoration(
+                                      hintText: 'First name *', isDense: true),
+                                  keyboardType: TextInputType.text,
+                                ),
+                                new TextFormField(
+                                  decoration: const InputDecoration(
+                                      hintText: 'Last name *', isDense: true),
+                                  keyboardType: TextInputType.text,
+                                ),
+                              ],
+                            )),
+                      )
+                    ],
+                  )),
             ),
+            // lta info header
             new Container(
-              padding: const EdgeInsets.only(top: 5.0),
-              child:
-            new Column(
-                 children: [
-                            new TextFormField(
-                              decoration: const InputDecoration(
-                                  hintText: 'Lta Number *', isDense: true),
-                              keyboardType: TextInputType.number,
-                            ),
-                            new DropdownButtonHideUnderline(
-            child: new Container(
-              decoration: new BoxDecoration(
-                border: new Border.all(
-                  color: const Color(0xFFD9D9D9),
-                ),
-              ),
-              child: new DropdownButton<int>(
-                items: <int>[0, 1].map((int value) {
-                  return new DropdownMenuItem<int>(
-                    value: value,
-                    child: new Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: new Text(value == 1? 'Male' : 'Female'),
+                padding: const EdgeInsets.only(left: 5.0, bottom: 5.0),
+                child: new Text('Lta Info')),
+            new Container(
+                color: Colors.white,
+                padding: const EdgeInsets.only(top: 5.0, left: 10.0),
+                child: new Column(
+                  children: [
+                    new TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: 'Lta Number *', isDense: true),
+                      keyboardType: TextInputType.number,
                     ),
-                  );
-                }).toList(),
-                value: quantity,
-                onChanged: (int value) { quantity = value;},
-              ),
-            ),
-          ),
-                          ],
-            )),
-            new Icon(Icons.search, size: 150.0, color: Colors.black12),
-            new Text('Edit Profile & Settings tab content')
+                  ],
+                )),
+            new Container(
+                padding: const EdgeInsets.only(top: 5.0, left: 10.0),
+                decoration: _topBottomBoxDecoration,
+                child: new Column(
+                  children: <Widget>[
+                    // Gender
+                    new _LabelIntDropDownItem(
+                        displayIntItems: [0, 1],
+                        label: 'Gender',
+                        output: selectedGender,
+                        displayFunc: (int i) => i == 0 ? 'Female' : 'Male'),
+                    // Miles from home
+                    new _LabelIntDropDownItem(
+                        displayIntItems: _distancesInMiles,
+                        label: 'Distance',
+                        output: selectedMiles,
+                        displayFunc: (int i) => '$i miles'),
+
+                    //  tournament Grade
+                    new _LabelIntDropDownItem(
+                        displayIntItems: _grades,
+                        label: 'Grade',
+                        output: selectedGrade,
+                        displayFunc: (int i) => 'Grade $i')
+                  ],
+                )),
           ],
         )));
   }
