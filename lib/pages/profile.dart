afd7b7a7dbc98dd5e49b126a9d9375ee302d41ea
Abdul Baruwa <abdulbaruwa@hotmail.com>
2017-12-26 09:02:08 +0000

    A little project structure restructure

---
+++ lib/pages/profile.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ProfileSection extends StatelessWidget {
+  const ProfileSection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => new Container(
+      color: Colors.amber,
+      child: new SizedBox(
+          height: 105.0,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Padding(
+                  padding: const EdgeInsets.only(left: 5.0, top: 5.0),
+                  child: new _UserProfile()),
+            ],
+          )));
+}
+
+// Displays the Vendor's name and avatar.
+class _UserProfile extends StatelessWidget {
+  const _UserProfile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizedBox(
+      height: 96.0,
+      child: new Row(
+        children: <Widget>[
+          new CircleAvatar(
+            radius: 36.0,
+            backgroundImage: const AssetImage('images/ademola.jpg'),
+          ),
+          const SizedBox(width: 8.0),
+          new Expanded(
+            child: new Padding(
+                padding: const EdgeInsets.only(left: 2.0, top: 25.0),
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    new Text('Ademola Baruwa'),
+                    new Text('99 Ashtead Avenue'),
+                    new Text('KT21 2XX, Surrey')
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/tabs/settings.dart
+++ lib/tabs/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../pages/profileEdit.dart';
+import '../pages/profile.dart';
 
 final ThemeData _kTheme = new ThemeData(
   brightness: Brightness.light,
@@ -12,7 +13,6 @@ final BoxDecoration _underlineBoxDecoration = new BoxDecoration(
     bottom: const BorderSide(color: const Color(0xFFD9D9D9)),
   ),
 );
-
 
 enum DismissDialogAction {
   cancel,
@@ -44,10 +44,6 @@ class SettingsTabState extends State<SettingsTab> {
                       builder: (BuildContext context) => new UserProfileEdit(),
                       fullscreenDialog: true,
                     ));
-
-                // scaffoldKey.currentState.showSnackBar(const SnackBar(
-                //   content: const Text('Not supported.'),
-                // ));
               },
             ),
             body: new Container(
@@ -55,62 +51,10 @@ class SettingsTabState extends State<SettingsTab> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                new _ProfileSection(),
+                new ProfileSection(),
                 new Icon(Icons.search, size: 150.0, color: Colors.black12),
                 new Text('Profile & Settings tab content')
               ],
             ))));
-  }
-}
-
-class _ProfileSection extends StatelessWidget {
-  const _ProfileSection({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => new Container(
-      color: Colors.amber,
-      child: new SizedBox(
-          height: 105.0,
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Padding(
-                  padding: const EdgeInsets.only(left: 5.0, top: 5.0),
-                  child: new _UserProfile()),
-            ],
-          )));
-}
-
-// Displays the Vendor's name and avatar.
-class _UserProfile extends StatelessWidget {
-  const _UserProfile({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new SizedBox(
-      height: 96.0,
-      child: new Row(
-        children: <Widget>[
-          new CircleAvatar(
-            radius: 36.0,
-            backgroundImage: const AssetImage('images/ademola.jpg'),
-          ),
-          const SizedBox(width: 8.0),
-          new Expanded(
-            child: new Padding(
-                padding: const EdgeInsets.only(left: 2.0, top: 25.0),
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    new Text('Ademola Baruwa'),
-                    new Text('66 West Farm Avenue')
-                  ],
-                )),
-          )
-        ],
-      ),
-    );
   }
 }
