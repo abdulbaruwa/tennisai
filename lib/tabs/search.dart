3ca1df80502ebd5554a6d8f88e9ac0c612e9ae5c
Abdul Baruwa <abdulbaruwa@hotmail.com>
2017-12-28 21:02:46 +0000

    Add button to search app bar

--- lib/tabs/search.dart
+++ lib/tabs/search.dart
@@ -8,33 +8,42 @@ final ThemeData _kTheme = new ThemeData(
   accentColor: Colors.redAccent,
 );
 
-class SearchTab extends StatefulWidget{
-  SearchTab ({Key key}) : super(key: key);  
+class SearchTab extends StatefulWidget {
+  SearchTab({Key key}) : super(key: key);
   @override
   SearchTabState createState() => new SearchTabState();
-} 
+}
 
 class SearchTabState extends State<SearchTab> {
-    static final GlobalKey<ScaffoldState> _scaffoldKey =
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
       new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return new Theme(
         data: _kTheme.copyWith(platform: Theme.of(context).platform),
-        
         child: new Scaffold(
-                appBar: new AppBar(
-        title: new Text(
-          'Tournament',
-          style: new TextStyle(
-            fontSize:
-                Theme.of(context).platform == TargetPlatform.iOS ? 17.0 : 20.0,
-          ),
-        ),
-        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-      ),
+            appBar: new AppBar(
+              actions: <Widget>[
+                new IconButton(
+                  icon: const Icon(Icons.vertical_align_bottom),
+                  tooltip: 'Sort by rating',
+                  onPressed: () {
+                    setState(() {});
+                  },
+                ),
+              ],
+              title: new Text(
+                'Tournament',
+                style: new TextStyle(
+                  fontSize: Theme.of(context).platform == TargetPlatform.iOS
+                      ? 17.0
+                      : 20.0,
+                ),
+              ),
+              elevation:
+                  Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+            ),
             key: _scaffoldKey,
-            body: new TournamentSearch()
-            ));
+            body: new TournamentSearch()));
   }
 }
