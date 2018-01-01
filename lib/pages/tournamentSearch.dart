bac09b958660995889c39b2b6f3768702fdfeeb7
Shangoman <abdulbaruwa@hotmail.com>
2018-01-01 10:24:28 +0000

    Added gesture recognition to search result and display standard tournament detail

--- lib/pages/tournamentSearch.dart
+++ lib/pages/tournamentSearch.dart
@@ -5,6 +5,13 @@ import '../models/tournament.dart' as _tournamentModels;
 import '../pages/touranmentDetails.dart';
 import "package:intl/intl.dart";
 
+final ThemeData _kTheme = new ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.indigo,
+  accentColor: Colors.redAccent,
+  //  platform: Theme.of(context).platform
+);
+
 class TournamentSearch extends StatefulWidget {
   const TournamentSearch({Key key}) : super(key: key);
   @override
@@ -15,9 +22,16 @@ class TournamentSearchState extends State<TournamentSearch> {
   @override
   Widget build(BuildContext context) {
     final List<Tournament> tournaments = _tournamentModels.tournaments;
-    Iterable<Widget> listTiles = tournaments
-        .map((Tournament item) => new TournamentCard(tournament: item));
-    listTiles = ListTile.divideTiles(context: context, tiles: listTiles, color: Theme.of(context).primaryColor);
+    Iterable<Widget> listTiles =
+        tournaments.map((Tournament item) => new TournamentCard(
+            tournament: item,
+            onTap: () {
+              showTournamentDetailsPage(context, item);
+            }));
+    listTiles = ListTile.divideTiles(
+        context: context,
+        tiles: listTiles,
+        color: Theme.of(context).primaryColor);
 
     final ThemeData themedata = Theme.of(context);
 
@@ -48,50 +62,65 @@ class TournamentCard extends StatelessWidget {
     return new GestureDetector(
       onTap: onTap,
       child: new Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Padding(
-              child: new ListTile(
-                title: new Text(tournament.name),
-                subtitle: new Text(tournament.location),
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Padding(
+            child: new ListTile(
+              title: new Text(tournament.name),
+              subtitle: new Text(tournament.location),
+            ),
+            padding: const EdgeInsets.only(
+                left: 1.0, right: 1.0, top: 1.0, bottom: 1.0),
+          ),
+          new Padding(
+            child: new Text(
+              tournamentDate,
+              textAlign: TextAlign.left,
+              //textScaleFactor: 1.0,
+            ),
+            padding: const EdgeInsets.only(left: 20.0),
+          ),
+          new SizedBox(
+            height: 5.0,
+          ),
+          new Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new Padding(
+                child: new Text("Grade ${tournament.grade}"),
+                padding: const EdgeInsets.only(left: 20.0),
               ),
-              padding: const EdgeInsets.only(
-                  left: 1.0, right: 1.0, top: 1.0, bottom: 1.0),
-            ),
-            new Padding(
-              child: new Text(
-                tournamentDate,
-                textAlign: TextAlign.left,
-                //textScaleFactor: 1.0,
-              ),
-              padding: const EdgeInsets.only(left: 20.0),
-            ),
-            new SizedBox(
-              height: 5.0,
-            ),
-            new Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                new Padding(
-                  child: new Text("Grade ${tournament.grade}"),
-                  padding: const EdgeInsets.only(left: 20.0),
+              new Expanded(
+                  child: new Padding(
+                child: new Text(
+                  "Entrants ${tournament.numberOfEntrants}",
+                  textAlign: TextAlign.end,
                 ),
-                new Expanded(
-                    child: new Padding(
-                  child: new Text(
-                    "Entrants ${tournament.numberOfEntrants}",
-                    textAlign: TextAlign.end,
-                  ),
-                  padding: const EdgeInsets.only(right: 10.0),
-                ))
-              ],
-            ),
-            new SizedBox(height: 15.0,)
-          ],
-        ),
-      
+                padding: const EdgeInsets.only(right: 10.0),
+              ))
+            ],
+          ),
+          new SizedBox(
+            height: 15.0,
+          )
+        ],
+      ),
     );
   }
+}
+
+void showTournamentDetailsPage(BuildContext context, Tournament tournament) {
+  Navigator.push(
+      context,
+      new MaterialPageRoute<Null>(
+        settings: const RouteSettings(name: '/tournament/details'),
+        builder: (BuildContext context) {
+          return new Theme(
+            data: _kTheme.copyWith(platform: Theme.of(context).platform),
+            child: new TournamentDetailsPage(tournament: tournament),
+          );
+        },
+      ));
 }
