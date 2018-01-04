c25cace345e80c2d71b2a4b03afdd90f5c37aac1
Shangoman <abdulbaruwa@hotmail.com>
2018-01-04 21:30:11 +0000

    Make basket item dismissable

--- lib/pages/basket.dart
+++ lib/pages/basket.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/tournament.dart';
 import '../pages/entrants.dart';
+import 'package:collection/collection.dart' show lowerBound;
+
 
 final ThemeData _kTheme = new ThemeData(
   brightness: Brightness.light,
@@ -19,6 +21,13 @@ class BasketPage extends StatefulWidget {
 class BasketPageState extends State<BasketPage> {
   static final GlobalKey<ScaffoldState> _scaffoldKey =
       new GlobalKey<ScaffoldState>();
+  List<Tournament> leaveBehindItems;
+
+  @override
+  void initState() {
+    super.initState();
+    leaveBehindItems = widget.tournaments;
+  }
 
   Widget buildListTile(BuildContext context, Tournament item) {
     Widget secondary;
@@ -43,17 +52,72 @@ class BasketPageState extends State<BasketPage> {
             ])),
         title: new Text(item.name),
         subtitle: new Text(item.status),
-        trailing: new ExcludeSemantics(
-            child: new Column(children: [
-          new Text('Ranking', style: new TextStyle(fontSize: 10.0)),
-          new CircleAvatar(
-            radius: 16.0,
-            child: new Text(item.grade,
-                style: Theme.of(context).primaryTextTheme.body1),
-          )
-        ])),
+        trailing: new Text('£${item.cost.toString()}'),
       ),
     );
+  }
+   void handleUndo(Tournament item) {
+    final int insertionIndex = lowerBound(leaveBehindItems, item);
+    setState(() {
+      leaveBehindItems.insert(insertionIndex, item);
+    });
+  }
+
+  Widget buildDismisableItem(Tournament item) {
+    final ThemeData theme = Theme.of(context);
+    return new Dismissible(
+        key: new ObjectKey(item),
+        direction: DismissDirection.horizontal,
+        onDismissed: (DismissDirection direction) {
+          setState(() {
+            leaveBehindItems.remove(item);
+          });
+          final String action = (direction == DismissDirection.endToStart)
+              ? 'archived'
+              : 'deleted';
+          _scaffoldKey.currentState.showSnackBar(new SnackBar(
+              content: new Text('You $action item ${item.name}'),
+              action: new SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    handleUndo(item);
+                  })));
+        },
+        background: new Container(
+            color: theme.primaryColor,
+            child: const ListTile(
+                leading:
+                    const Icon(Icons.delete, color: Colors.white, size: 36.0))),
+        secondaryBackground: new Container(
+            color: theme.primaryColor,
+            child: const ListTile(
+                trailing: const Icon(Icons.archive,
+                    color: Colors.white, size: 36.0))),
+        child: new Container(
+            decoration: new BoxDecoration(
+                color: theme.canvasColor,
+                border: new Border(
+                    bottom: new BorderSide(color: theme.dividerColor))),
+            child: new MergeSemantics(
+              child: new ListTile(
+                dense: true,
+                leading: new ExcludeSemantics(
+                    child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                      new Text('Rating', style: new TextStyle(fontSize: 10.0)),
+                      new CircleAvatar(
+                          radius: 16.0,
+                          child: new Text(
+                            item.grade,
+                            style: Theme.of(context).primaryTextTheme.body1,
+                          ))
+                    ])),
+                title: new Text(item.name),
+                subtitle: new Text(item.status),
+                trailing: new Text('£${item.cost.toString()}'),
+              ),
+            )));
   }
 
   Widget buildTotalRow(BuildContext context, double totalAmount) {
@@ -78,12 +142,16 @@ class BasketPageState extends State<BasketPage> {
   Widget build(BuildContext context) {
     List<Tournament> tournamentEntrants = widget.tournaments;
     Iterable<Widget> listTiles = tournamentEntrants
-        .map((Tournament item) => buildListTile(context, item));
+        .map((Tournament item) => buildDismisableItem(item));
     listTiles = ListTile.divideTiles(context: context, tiles: listTiles);
     return new Column(children: <Widget>[
       new SizedBox(
         height: 56.0,
         child: new Row(children: <Widget>[
+          new CircleAvatar(
+            radius: 26.0,
+            backgroundImage: const AssetImage('images/ademola.jpg'),
+          ),
           new Expanded(
             child: new Padding(
                 padding: const EdgeInsets.only(left: 2.0, top: 5.0),
@@ -92,8 +160,8 @@ class BasketPageState extends State<BasketPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     new Text('Ademola Baruwa'),
-                    new Text('99 Ashtead Avenue'),
-                    new Text('KT21 2XX, Surrey')
+                    new Text('LTA Number: 125587'),
+                    // new Text('KT21 2XX, Surrey')
                   ],
                 )),
           ),
